a92f62053fa4c2cabe5b6f399d7e6cca3185b55e
vijayst <vijayst@gmail.com>
2016-07-18 00:16:02 +0530

    integrating esri map into react.. trying!

--- app/client/imports/common/map.jsx
+++ app/client/imports/common/map.jsx
@@ -1,26 +1,7 @@
 import React from 'react';
-import arcGIS from 'arcgis';
-
-function print(...args) {
-  console.log(...args);
-}
 
 export default class Map extends React.Component {
-
-  componentDidMount() {
-    const arc = arcGIS();
-    arc.request()
-    .then(print)
-    .catch(print);
-
-    arc.map('3b93337983e9436f8db950e38a8629af')
-    .then(print)
-    .catch(print);
-  }
-
   render() {
-    return (
-      <div />
-    );
+    return null;
   }
 }
