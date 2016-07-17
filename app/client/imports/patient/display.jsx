4bb4ed578bbd1a8306966321a51cba40a43d0446
vijayst <vijayst@gmail.com>
2016-07-17 18:39:36 +0530

    adding a Map component.

--- app/client/imports/patient/display.jsx
+++ app/client/imports/patient/display.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
+import Map from '../common/map';
 
 export default class Display extends React.Component {
   render() {
     return (
       <div>
-      {this.props.volunteers.length > 0 ? (
-        this.props.volunteers.map((v) => (
-          <div key={v.name} className="bg-info text-info volunteer">
-          {v.name}
-          </div>
-        ))
-      ) : null}
+        <Map />
+        {this.props.volunteers.length > 0 ? (
+          this.props.volunteers.map((v) => (
+            <div key={v.name} className="bg-info text-info volunteer">
+            {v.name}
+            </div>
+          ))
+        ) : null}
       </div>
     );
   }
